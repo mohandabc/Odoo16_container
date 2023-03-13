c0be9d247d2c8f0ddb4e941be5ed9af55a71418b
Mohand <mohand_abchiche@live.fr>
2023-03-13 10:43:41 +0100

    initial commit

---
+++ addons/my_module/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'My Module',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'summary': 'Module for test',
+    'description': """
+        Module to test if extra addons are read
+    """,
+    'author': 'Mohand',
+    'depends': [],
+    'data': [
+        # List of XML or CSV files that contain data for your module
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application':True,
+}
